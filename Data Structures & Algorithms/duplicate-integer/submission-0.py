359817d0484696072f80971b638d15598a33807e
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-05-03 13:43:40 +0900

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+        
